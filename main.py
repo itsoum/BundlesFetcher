68e3eae86d2f5cb81af087b3c3409af74ca5d4c1
Ilias Tsoumas <tsoumilion@gmail.com>
2018-06-20 15:27:43 +0300

    Dockerfile + minor changes

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pycurl
-import certifi
-from StringIO import StringIO
+from io import BytesIO
 
-buffer = StringIO()
+buffer = BytesIO()
 c = pycurl.Curl()
 c.setopt(c.URL, 'https://api.jujucharms.com/charmstore/v5/list?type=bundle')
 c.setopt(c.WRITEDATA, buffer)
-c.setopt(c.CAINFO, certifi.where())
 c.perform()
 c.close()
 
 body = buffer.getvalue()
-print(body)
+# Body is a byte string.
+# We have to know the encoding in order to print it to a text file
+# such as standard output.
+print(body.decode('iso-8859-1'))
