64b1c1fb218a4e8e071176b99b99487edf981c51
Ilias Tsoumas <tsoumilion@gmail.com>
2018-06-21 17:44:20 +0300

    fetching bundle.yaml

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import urllib.request
 import re
+import requests
+import yaml
 
 def bundles_link_part(bundle_name):
   return re.findall(r'[\w]*[^cs:]', bundle_name)
@@ -19,20 +21,16 @@ def bundles_list():
 	    #print(j)
 	    return bundle_list
 
- https://api.jujucharms.com/charmstore/v5/bundle/<bundle-name>/archive/bundle.yaml
 
-with urllib.request.urlopen("https://api.jujucharms.com/charmstore/v5/list?type=bundle") as url:
-	    data = json.loads(url.read().decode())
-	    j=0
-	    bundle_list=[]
-	    for i in data['Results']:
-	    	bundle_name = ''.join(bundles_link_part(data['Results'][j]['Id']))
-	    	bundle_list.append(bundle_name)
-	    	#print(bundle_name)
-	    	j+=1
-	    #print(bundle_list)
-	    #print(j)
-	    return bundle_list
+bundles=bundles_list()
+
+#https://api.jujucharms.com/charmstore/v5/bundle/<bundle-name>/archive/bundle.yaml
+for bundle in bundles:
+	print(bundle)
+	resp=urllib.request.urlopen('https://api.jujucharms.com/charmstore/v5/' + bundle + '/archive/bundle.yaml')
+	data = yaml.load(resp)
+	print(data)
+	#data = json.loads(x.read().decode())
 
 
 
