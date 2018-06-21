b5393c85a025294251089fdb6620665fb4673275
Ilias Tsoumas <tsoumilion@gmail.com>
2018-06-21 16:53:32 +0300

    grabber of the useful part for the bundle's title to get it from api with the proper api request per bundle

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 import json
 import urllib.request
+import re
 
+def bundles_link_part(bundle_name):
+  return re.findall(r'[\w]*[^cs:]', bundle_name)
+
+def bundles_list():
+	with urllib.request.urlopen("https://api.jujucharms.com/charmstore/v5/list?type=bundle") as url:
+	    data = json.loads(url.read().decode())
+	    j=0
+	    bundle_list=[]
+	    for i in data['Results']:
+	    	bundle_name = ''.join(bundles_link_part(data['Results'][j]['Id']))
+	    	bundle_list.append(bundle_name)
+	    	#print(bundle_name)
+	    	j+=1
+	    #print(bundle_list)
+	    #print(j)
+	    return bundle_list
+
+ https://api.jujucharms.com/charmstore/v5/bundle/<bundle-name>/archive/bundle.yaml
 
 with urllib.request.urlopen("https://api.jujucharms.com/charmstore/v5/list?type=bundle") as url:
-    data = json.loads(url.read().decode())
-    j=0
-    for i in data['Results']:
-    	print(data['Results'][j]['Id'])
-    	j+=1
-    print(j)
+	    data = json.loads(url.read().decode())
+	    j=0
+	    bundle_list=[]
+	    for i in data['Results']:
+	    	bundle_name = ''.join(bundles_link_part(data['Results'][j]['Id']))
+	    	bundle_list.append(bundle_name)
+	    	#print(bundle_name)
+	    	j+=1
+	    #print(bundle_list)
+	    #print(j)
+	    return bundle_list
+
 
 
