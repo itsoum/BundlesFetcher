377b0db9b4b82a7b5132f454c01c8ac8a806cfac
Ilias Tsoumas <tsoumilion@gmail.com>
2018-10-29 14:22:03 +0200

    Generation of a links.csv dataset filled with graphs' links between nodes by the JUJU bundles

--- graph_exporter.py
+++ graph_exporter.py
@@ -3,38 +3,105 @@ import urllib.request
 import re
 import requests
 import yaml
-from collections import Counter
 import itertools
-
 import pandas as pd
 import numpy as np
+import csv
+import os
+from collections import Counter
 
-#DAG
+#-----------------------------------------------------------------------
+# CSV files
+#  - links for the direct relations per graph (adjacent matrix)
+#  - gdistances for the Djikstra shortest paths
+#    between all nodes per graph (distances matrix)
+#-----------------------------------------------------------------------
+outfile_links = "links.csv"
+outfile_gdist = "gdistances.csv"
 
-#uDAG
+#-----------------------------------------------------------------------
+# open two files to write (mode "w"), and create two CSV writer objects
+#-----------------------------------------------------------------------
+csvfile_links = open(outfile_links, "w")
+csvwriter_links = csv.writer(csvfile_links)
 
-with open('bundles_yaml/django-2.yml') as bundle:
-	data = yaml.load(bundle)
-	print(data['relations'])
-	i=0
-	relations = [r for r in data['relations']]
-	print(relations[0][1])
+csvfile_gdist = open(outfile_gdist, "w")
+csvwriter_gdist = csv.writer(csvfile_gdist)
 
-	final = list(itertools.chain.from_iterable(relations))
-	print(final)
-
-	for f in final:
-		#chname=f.rsplit(':',1)[-1]
-		#print(chname)
-		chname=f.rsplit(':',1)[0]
-		print(chname)
-		print(data['services'][chname]['charm'])
+#-----------------------------------------------------------------------
+# add headings to CSV files
+#-----------------------------------------------------------------------
+row = [ "graph", "component", "component" ]
+csvwriter_links.writerow(row)
+row = [ "graph", "component", "component", "distance" ]
+csvwriter_gdist.writerow(row)
 
 
-	am = pd.DataFrame(np.zeros(shape=(int(len(final)/2),int(len(final)/2))))
-	print(am)	
-	
-	#relations
-	#print(data['services'][relations]['charm'])
+#-----------------------------------------------------------------------
+# JUJU Dataset formation
+# considering each bundle as uDAG (undirected graph)
+#-----------------------------------------------------------------------
+
+j=0
+k=0
+k1=[]
+# directory of descriptors
+path = "bundles_yaml/"
+# list of bundles
+filelist = os.listdir(path)
+for i in filelist:
+	print(path+i)
+	j+=1
+	print(j)
+	with open(path+i) as bundle:
+		
+		data = yaml.load(bundle)
+		try:
+			relations = [r for r in data['relations']]
+			final = list(itertools.chain.from_iterable(relations))
+			print(final)
+			listcharmfullname=[]
+			flag=1
+			for f in final:
+
+				# Service of charm
+				chname=f.rsplit(':',1)[0]
+				print(chname)
+
+				# Endpoint of charm
+				'''
+				chservicename=f.rsplit(':',1)[1]
+				print(chservicename)
+				'''
+
+				try:
+					charmfullname=data['services'][chname]['charm']
+				except:
+					charmfullname=data['applications'][chname]['charm']
+
+				listcharmfullname.append(charmfullname.rsplit('-',1)[0]) 
+				
+				# Charm full name
+				finalcharm=charmfullname.rsplit('-',1)[0]
+				print(finalcharm)
+
+				if(flag%2==0):
+					
+					if(tempfinalcharm == finalcharm):
+						tempfinalcharm=tempfinalcharm+'/'+tempchservicename
+						finalcharm=finalcharm+'/'+chname
+					# Write new row on links.csv
+					row = [ i, tempfinalcharm, finalcharm]
+					csvwriter_links.writerow(row)
+				else:
+					tempfinalcharm=finalcharm
+					tempchservicename=chname
+				flag+=1
+		except:
+			k1.append(path+i)
+
+			k+=1
+print('ta leme',k,k1)
+
 
 
